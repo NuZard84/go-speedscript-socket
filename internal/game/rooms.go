5da073438fdfabfda9d22d331adcb5a02e76c5c2
NuZrd84 <86848690+NuZard84@users.noreply.github.com>
2025-02-02 02:00:48 +0530

    update notyfy kicked player socket signal

--- internal/game/rooms.go
+++ internal/game/rooms.go
@@ -640,7 +640,7 @@ func (room *Room) KickPlayer(targetUsername string, adminClient *Client) error {
 	}
 
 	kickMsg := models.Message{
-		Type: "kick_player",
+		Type: "you_are_kicked",
 		Data: map[string]interface{}{
 			"message": "You have been kicked from the room",
 			"by":      adminClient.Username,
